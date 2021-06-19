4cf3d775d49b1f9b497935f0880ff03d8d3a016c
Nurazmi <azmimr67@gmail.com>
2021-06-19 17:12:45 +0700

    Pre commit

--- letter_management/__openerp__.py
+++ letter_management/__openerp__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # Copyright 2020 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Letter Management",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "license": "AGPL-3",
